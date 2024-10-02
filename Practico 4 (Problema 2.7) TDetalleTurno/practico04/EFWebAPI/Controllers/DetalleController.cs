bbf90c510a0c12c5b9c26ac923eb69aa20811b3a
Gino <412045@tecnicatura.frc.utn.edu.ar>
2024-10-02 12:42:58 -0300

    cambios de nombre

---
+++ Practico 4 (Problema 2.7) TDetalleTurno/practico04/EFWebAPI/Controllers/DetalleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetalleController : ControllerBase
+    {
+    }
+}
